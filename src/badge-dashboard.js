e562d67a23e3093043afec3c17900ce02454f0d7
NewJerkey <bryschneider1@gmail.com>
2023-03-21 13:03:22 -0400

    there is now a search bar

--- src/badge-dashboard.js
+++ src/badge-dashboard.js
@@ -1,88 +1,53 @@
-import { LitElement, html, css } from 'lit';
+import { LitElement, html, css } from "lit";
+import "@lrnwebcomponents/simple-icon/simple-icon.js";
+import "@lrnwebcomponents/simple-icon/lib/simple-icons.js";
 
-const logo = new URL('../assets/open-wc-logo.svg', import.meta.url).href;
+const logo = new URL("../assets/open-wc-logo.svg", import.meta.url).href;
 
 class BadgeDashboard extends LitElement {
   static properties = {
     header: { type: String },
-  }
+  };
 
   static styles = css`
-    :host {
-      min-height: 100vh;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: flex-start;
-      font-size: calc(10px + 2vmin);
-      color: #1a2b42;
-      max-width: 960px;
-      margin: 0 auto;
-      text-align: center;
-      background-color: var(--badge-dashboard-background-color);
+    .searchbar {
+      box-shadow: 1px 1px 1px 2px gray;
+      margin: auto;
+      margin-top: 5px;
+      width: fit-content;
+      
     }
-
-    main {
-      flex-grow: 1;
+    .searchInput {
+      width: 1300px;
+      height: 50px;
+      border: 0;
     }
-
-    .logo {
-      margin-top: 36px;
-      animation: app-logo-spin infinite 20s linear;
-    }
-
-    @keyframes app-logo-spin {
-      from {
-        transform: rotate(0deg);
-      }
-      to {
-        transform: rotate(360deg);
-      }
-    }
-
-    .app-footer {
-      font-size: calc(12px + 0.5vmin);
-      align-items: center;
-    }
-
-    .app-footer a {
-      margin-left: 5px;
+    .icon{
+      width: 45px;
     }
   `;
 
   constructor() {
     super();
-    this.header = 'My app';
+    this.header = "My app";
   }
 
   render() {
     return html`
       <main>
-        <div class="logo"><img alt="open-wc logo" src=${logo} /></div>
-        <h1>${this.header}</h1>
-
-        <p>Edit <code>src/BadgeDashboard.js</code> and save to reload.</p>
-        <a
-          class="app-link"
-          href="https://open-wc.org/guides/developing-components/code-examples/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Code examples
-        </a>
+        <div class="searchbar">
+          <form>
+            <simple-icon icon="search" class="icon"></simple-icon>
+            <input
+              class="searchInput"
+              type="text"
+              placeholder="Search Content, Topics, and People"
+            />
+          </form>
+        </div>
       </main>
-
-      <p class="app-footer">
-        🚽 Made with love by
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://github.com/open-wc"
-          >open-wc</a
-        >.
-      </p>
     `;
   }
 }
 
-customElements.define('badge-dashboard', BadgeDashboard);
+customElements.define("badge-dashboard", BadgeDashboard);
